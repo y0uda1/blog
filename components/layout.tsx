19762e912db6bf9bc676d80769ef03dc48af8807
y0uda1 <y0uda1.0609@gmail.com>
2021-04-19 18:05:59 +0900

    githubのリンク追加とprettier eslintの設定

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,24 +1,39 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Layout({
     children,
-    title = 'y0udai blog'
+    title = 'y0udai.dev'
 }) {
-    title = 'ここにタイトル'
     return (
         <div className={'min-h-screen flex flex-col bg-indigo-200'}>
             <Head>
                 <title>{title}</title>
             </Head>
-            <header className={'h-11 flex flex-wrap content-center bg-indigo-50'}>
-                <span className={'px-8'}>{title}</span>
-                <nav className={'flex flex-row'}>
+            <header className={'flex flex-wrap justify-center bg-indigo-50'}>
+                <nav className={'h-10 flex items-stretch'}>
                     <Link href='/'>
-                        <a className={'px-2'}>{'introduction'}</a>
+                        <a className={'px-2 flex flex-wrap content-center text-lg font-bold hover:text-gray-400'}>
+                            <span>introduction</span>
+                        </a>
                     </Link>
                     <Link href='/blog'>
-                        <a className={'px-2'}>{'blog'}</a>
+                        <a className={'px-2 flex flex-wrap content-center text-lg font-bold hover:text-gray-400'}>blog</a>
+                    </Link>
+                    <Link href={'https://github.com/y0uda1'}>
+                        <a className={'px-2 flex flex-wrap content-center text-lg font-bold hover:text-gray-400'}>
+                            <div>
+                                <Image
+                                    src='/GitHub-Mark-32px.png'
+                                    alt='github icon'
+                                    width={16}
+                                    height={16}
+                                    layout={'fixed'}
+                                />
+                                <span className={'pl-1'}>GitHub</span>
+                            </div>
+                        </a>
                     </Link>
                 </nav>
             </header>
@@ -26,10 +41,6 @@ export default function Layout({
             <div className={'flex-grow container mx-auto'}>
                 {children}
             </div>
-
-            <footer className={'flex-col bg-indigo-50'}>
-                <span>{'ここにフッター'}</span>
-            </footer>
         </div>
     )
 }
